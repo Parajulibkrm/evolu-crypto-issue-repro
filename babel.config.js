fb1ad470292599de1fc2e15f56016fdfc4e0d4a6
Bikram Parajuli <bikramparajuli0000@gmail.com>
2025-08-10 11:38:03 +0545

    chore: Added babel module resolver plugin

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
+  let plugins = []
 
   plugins.push('react-native-reanimated/plugin');
+  plugins.push('@babel/plugin-transform-dynamic-import');
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
